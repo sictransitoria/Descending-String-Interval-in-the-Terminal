b6dfef740b896545ba6b341bea6399080740e08b
Michael J. Oria <michael_oria@me.com>
2022-02-01 21:38:24 -0500

    Updates readability of code, removes silly "Hello There" string

--- stars.js
+++ stars.js
@@ -1,29 +1,22 @@
-// Let's shine (*)
+// Let's Shine.
 
 var stars = ("* ").repeat(10); // Set variable to repeat *'s
 var space = setInterval(function(){ // Set function to create descending *'s from 10 down to 1.
-  stars = stars.split(" "); // Put a space in between *'s upon first print out.
-  stars.pop(); // .pop() removes the last *, and returns that element.  
-  stars = stars.join(" "); // joins *'s, and returns the string
-  	console.log(stars); // print *'s to the console.
+    stars = stars.split(" "); // Put a space in between *'s upon first print out.
+    stars.pop(); // .pop() removes the last *, and returns that element.  
+    stars = stars.join(" "); // joins *'s, and returns the string
+//   	console.log(stars); // print *'s to the console.
+  
   if(stars.length === 1) {
   	clearInterval(space); // If the value of the stars variable is equal to one, stop the function.
-  
   }
-
 });
 
-// What time is it? Descending time.
-
 function descendingTime(a, b) { // Set function for print out. A, B are placeholders.
-  let string = "Hello there, " // Say Hello to each * as they descend (:
-  for(let i = 0; i < b; i++){ // Loop to print out *'s 
-  	string = string + a; 
-  
+  for(var i = 0; i < b; i++){ // Loop to print out *'s 
+  	a = a + stars;
   }
-
-  return string; // This return will print out 10 *'s.
-
+  return a; // This return will print out 10 *'s.
 }
 
 var number = 11; // Set variable to start at 11.
@@ -33,6 +26,5 @@ var printOut = setInterval(function(){
 }, 1000);
   
   setTimeout(function(){
-  	clearInterval(printOut)
-
+  clearInterval(printOut)
 }, 11000); // Write out *'s for 11 seconds
