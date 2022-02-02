f07b078bd2f11a4e70c489b207ccaf47dd77546b
Michael J. Oria <michael_oria@me.com>
2022-02-02 07:23:09 -0500

    One tiny change.

--- stars.js
+++ stars.js
@@ -5,15 +5,15 @@ var space = setInterval(function(){ // Set function to create descending *'s fro
     stars = stars.split(" "); // Put a space in between *'s upon first print out.
     stars.pop(); // .pop() removes the last *, and returns that element.  
     stars = stars.join(" "); // joins *'s, and returns the string
-//   	console.log(stars); // print *'s to the console.
+    console.log(stars);
   
-  if(stars.length === 1) {
+  if (stars.length == 1) {
   	clearInterval(space); // If the value of the stars variable is equal to one, stop the function.
   }
 });
 
 function descendingTime(a, b) { // Set function for print out. A, B are placeholders.
-  for(var i = 0; i < b; i++){ // Loop to print out *'s 
+  for(var i = 0; i < b; i++) { // Loop to print out *'s 
   	a = a + stars;
   }
   return a; // This return will print out 10 *'s.
@@ -25,6 +25,6 @@ var printOut = setInterval(function(){
   console.log(descendingTime("*", number)); // print number variable using *'s to console. 
 }, 1000);
   
-  setTimeout(function(){
+  setTimeout(function() {
   clearInterval(printOut)
 }, 11000); // Write out *'s for 11 seconds
